26a18397a439d376ebe5b6914c60b9f2cbf58652
xiaorongnie <xiaorongnie@transcodegroup.com>
2022-01-04 15:40:27 +0800

    WAV数据接收

--- src/main/java/cn/org/hentai/jtt1078/ws/WsServerEndpoint.java
+++ src/main/java/cn/org/hentai/jtt1078/ws/WsServerEndpoint.java
@@ -17,7 +17,7 @@ import cn.org.hentai.jtt1078.util.Utils;
 import lombok.extern.slf4j.Slf4j;
 
 /**
- * WebSocket对讲
+ * WebSocket对讲, 默认接收WAV格式数据,每一包数据都封装了WAV头部数据
  * 
  * @param imei
  *            设备号
@@ -29,13 +29,6 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class WsServerEndpoint {
 
-    /**
-     * 连接成功
-     *
-     * @param session
-     * @param imei
-     *            设备号
-     */
     @OnOpen
     public void onOpen(Session session, @PathParam(value = "imei") String imei) {
         session.getUserProperties().put("imei", Utils.formatPhoneNumber(imei));
@@ -43,22 +36,12 @@ public class WsServerEndpoint {
         log.info("WebSocket open -> {}, {}", session.getId(), imei);
     }
 
-    /**
-     * 连接关闭
-     *
-     * @param session
-     */
     @OnClose
     public void onClose(Session session) {
         log.info("WebSocket close -> {} {}", session.getId(), session.getUserProperties().get("imei"));
         WsSessionGroup.remove(session);
     }
 
-    /**
-     * 接收到消息
-     *
-     * @param text
-     */
     @OnMessage
     public void onMsg(byte[] data, Session session) {
         String imei = String.valueOf(session.getUserProperties().get("imei"));
@@ -67,16 +50,9 @@ public class WsServerEndpoint {
         PublishManager.getInstance().publishAudio(Arrays.copyOfRange(data, 44, data.length), imei);
     }
 
-    /**
-     * 异常处理
-     * 
-     * @param session
-     * @param error
-     */
     @OnError
     public void onError(Session session, Throwable error) {
         log.error("WebSocket error " + session.getId(), error);
-        error.printStackTrace();
     }
 
 }
