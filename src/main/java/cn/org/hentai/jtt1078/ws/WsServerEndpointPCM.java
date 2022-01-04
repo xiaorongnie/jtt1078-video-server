2a084c3403fd28256f3103eea5296d68b3554580
xiaorongnie <xiaorongnie@transcodegroup.com>
2022-01-04 15:36:05 +0800

    接收PCM数据

--- src/main/java/cn/org/hentai/jtt1078/ws/WsServerEndpointPCM.java
+++ src/main/java/cn/org/hentai/jtt1078/ws/WsServerEndpointPCM.java
@@ -10,7 +10,6 @@ import javax.websocket.server.ServerEndpoint;
 
 import org.springframework.stereotype.Component;
 
-import cn.org.hentai.jtt1078.codec.ffmpeg.aac.AACCodec;
 import cn.org.hentai.jtt1078.entity.MediaEncoding;
 import cn.org.hentai.jtt1078.publisher.PublishManager;
 import cn.org.hentai.jtt1078.util.Utils;
@@ -25,62 +24,33 @@ import lombok.extern.slf4j.Slf4j;
  * @date 2021/04/30
  */
 @Component
-@ServerEndpoint("/talk/aac/{imei}")
+@ServerEndpoint("/talk/pcm/{imei}")
 @Slf4j
-public class WsServerEndpoint2 {
+public class WsServerEndpointPCM {
 
-    /**
-     * 连接成功
-     *
-     * @param session
-     * @param imei
-     *            设备号
-     */
     @OnOpen
     public void onOpen(Session session, @PathParam(value = "imei") String imei) {
-        log.info("WebSocket open -> {}, {}", session.getId(), imei);
+        log.info("WebSocket open (PCM 8k 16bit) -> {}, {}", session.getId(), imei);
         session.getUserProperties().put("imei", Utils.formatPhoneNumber(imei));
-        session.getUserProperties().put("encoding", MediaEncoding.Encoding.AAC.toString());
-        session.getUserProperties().put("codec", new AACCodec());
+        session.getUserProperties().put("encoding", MediaEncoding.Encoding.PCM_AUDIO.toString());
         WsSessionGroup.put(session);
     }
 
-    /**
-     * 连接关闭
-     *
-     * @param session
-     */
     @OnClose
     public void onClose(Session session) {
-        log.info("WebSocket close -> {} {}", session.getId(), session.getUserProperties().get("imei"));
+        log.info("WebSocket close (PCM 8k 16bit) -> {} {}", session.getId(), session.getUserProperties().get("imei"));
         WsSessionGroup.remove(session);
     }
 
-    /**
-     * 接收到消息
-     *
-     * @param text
-     */
     @OnMessage
     public void onMsg(byte[] data, Session session) {
         String imei = String.valueOf(session.getUserProperties().get("imei"));
-        Object codec = String.valueOf(session.getUserProperties().get("codec"));
-        if (codec != null && codec instanceof AACCodec) {
-            AACCodec aacCodec = (AACCodec)codec;
-            PublishManager.getInstance().publishAudio(aacCodec.toPCM(data), imei);
-        }
+        PublishManager.getInstance().publishAudio(data, imei);
     }
 
-    /**
-     * 异常处理
-     * 
-     * @param session
-     * @param error
-     */
     @OnError
     public void onError(Session session, Throwable error) {
         log.error("WebSocket error " + session.getId(), error);
-        error.printStackTrace();
     }
 
 }
